7f9eb5b859c91a5dd81f1bf4c66ab8f7bc975a37
Artem <milkartem2@mail.ru>
2024-10-01 22:44:53 +0300

    v3

--- main.py
+++ main.py
@@ -1,35 +1,45 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
-# Шаг 1: Загрузка изображения графика
+# Шаг 1: Загрузка изображения
 image = cv2.imread('graphs/CoFe2O4.png')
 
-# Преобразуем изображение в градации серого
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# Преобразуем изображение в цветовое пространство HSV
+hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-# Применим бинаризацию для лучшего выделения графика
-_, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY_INV)
+# Установим границы для черного цвета (черная линия гистерезиса)
+lower_black = np.array([0, 0, 0])
+upper_black = np.array([180, 255, 50])
 
-# Используем Canny для выделения краев графика
-edges = cv2.Canny(thresh, 50, 150)
+# Создаем маску для выделения только черных пикселей (линия гистерезиса)
+black_mask = cv2.inRange(hsv_image, lower_black, upper_black)
 
-# Шаг 2: Найдем контуры на изображении (чтобы выделить график)
-contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+# Убираем мелкие объекты (подписи и текст) с помощью морфологических операций
+kernel = np.ones((5, 5), np.uint8)
+cleaned_mask = cv2.morphologyEx(black_mask, cv2.MORPH_OPEN, kernel)
 
-# Создадим копию изображения для отображения контуров
-image_contours = image.copy()
+# Обрезаем края, чтобы удалить оси
+height, width = cleaned_mask.shape
+crop_mask = cleaned_mask[int(0.1*height):int(0.9*height), int(0.1*width):int(0.9*width)]
 
-# Нарисуем найденные контуры
-cv2.drawContours(image_contours, contours, -1, (255, 0, 0), 1)
+# Применяем маску к изображению, чтобы получить только черные области
+black_line = cv2.bitwise_and(image, image, mask=cleaned_mask)
 
-# Отображение контуров на графике
+# Покажем результат маскирования (выделение черной линии)
 plt.figure(figsize=(10, 6))
-plt.imshow(cv2.cvtColor(image_contours, cv2.COLOR_BGR2RGB))
-plt.title("Найденные контуры на изображении")
+plt.imshow(cv2.cvtColor(black_line, cv2.COLOR_BGR2RGB))
+plt.title("Очищенная черная линия гистерезиса без осей и подписей")
 plt.show()
 
-# Шаг 3: Выделение точек графика
+# Шаг 2: Преобразование выделенной линии в данные x и y
+gray = cv2.cvtColor(black_line, cv2.COLOR_BGR2GRAY)
+_, thresh = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)
+
+# Найдем контуры для выделенной линии
+contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
 # Собираем координаты точек графика
 graph_points = []
 for contour in contours:
@@ -37,8 +47,7 @@ for contour in contours:
         x, y = point[0]
         graph_points.append((x, y))
 
-# Шаг 4: Преобразование пикселей в координаты графика (с учетом диапазона осей)
-# Пример: допустим диапазон оси X [-40, 60] и оси Y [-60, 60]
+# Шаг 3: Преобразование пикселей в реальные значения
 x_range = (-40, 60)  # Диапазон оси X
 y_range = (-60, 60)  # Диапазон оси Y
 
@@ -52,31 +61,18 @@ for (x, y) in graph_points:
     real_y = y_range[1] - (y / height) * (y_range[1] - y_range[0])
     graph_data.append((real_x, real_y))
 
-# # Шаг 5: Запись данных в Excel
+# # Запись данных в Excel
 # df = pd.DataFrame(graph_data, columns=['X', 'Y'])
-# df.to_excel('graph_data.xlsx', index=False)
+# df.to_excel('data/graph_data.xlsx', index=False)
 
-# print("Данные графика успешно сохранены в файл 'graph_data.xlsx'.")
-
-# Шаг 6: Построение восстановленного графика
+# Построение восстановленного графика
 x_values = [point[0] for point in graph_data]
 y_values = [point[1] for point in graph_data]
 
-# Шаг 7: Отображение исходного и восстановленного графика для сравнения
-plt.figure(figsize=(12, 6))
-
-# Исходное изображение
-plt.subplot(1, 2, 1)
-plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-plt.title("Исходное изображение графика")
-
-# Восстановленный график
-plt.subplot(1, 2, 2)
+plt.figure(figsize=(6, 6))
 plt.plot(x_values, y_values, 'b-', label='Восстановленный график')
 plt.title("Восстановленный график по данным")
 plt.xlabel('H, kOe')
 plt.ylabel('M, emu/g')
 plt.legend()
-
-# Отображаем оба графика для сравнения
 plt.show()
